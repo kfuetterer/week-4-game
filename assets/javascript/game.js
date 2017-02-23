9063433c0663a048bc7e5df8a8a72d6f458f12e2
Katharina Fuetterer <Katharina Fuetterer>
2017-02-22 22:42:10 -0500

    Update 02/22/2017 1042

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,19 @@
-var hp = [120, 100, 150, 180];
+var hp = [120, 100, 180, 150];
 var ap = [4, 6, 8, 10];
-var cap = [5, 10, 15, 25];
+var cap = [5, 10, 20, 15];
+var characters = ["Finn", "Rey", "Kylo Ren", "Captain Phasma"];
+var defeated = [0, 0, 0, 0];
+var win = 0;
 
 var z = "0";
 var w = "0";
 var a = 0;
 var apb = 0;
 
-
 window.onload = function() {
 	for (i = 0; i < hp.length; i++) {
-	var html = "<p>" + hp[i] + "</p>";
-	document.querySelector(".h" + (i + 1)).innerHTML = html;
+		var html = "<p>" + hp[i] + "</p>";
+		document.querySelector(".h" + (i + 1)).innerHTML = html;
 	}
 
 	for (i = 0; i < hp.length; i++) {
@@ -35,7 +37,6 @@ function reply_click(clicked_id) {
 			}
 		}
 	}
-
 }
 
 function reply_clicktwo(clicked_id) {
@@ -56,9 +57,14 @@ function reply_clicktwo(clicked_id) {
 	}
 }
 
-
-
 function Attack() {
+
+	if ((z === "0") || (w === "0")) {
+		var htmlllllll = "<p>Please select a character and/or enemy.</p>";
+		document.querySelector("#pointText").innerHTML = htmlllllll;
+		return;
+	}
+
 	a++;
 
 	var x = parseInt(w) - 5;
@@ -68,22 +74,102 @@ function Attack() {
 
 	hp[x] = hp[x] - (ap[y] * apb);
 
-	if (hp[x] < 0) {
+	if (hp[x] <= 0) {
 		hp[x] = 0;
 		a = 0;
+		win++;
+		document.getElementById("img" + w).style.opacity = "1";
+		document.getElementById(w + w).style.opacity = "0.5";
+		var htmlll = "<p>You defeated " + characters[x] + ". Choose another enemy.</p>";
+		document.querySelector("#gameover").innerHTML = htmlll;
 	}
 
-	if (hp[x] > 0) {
+	if (win === 3) {
+		var htmlll = "<p>You Win!</p>";
+		document.querySelector("#gameover").innerHTML = htmlll;
+
+			$("#restartButton").on("click",function() {
+				hp = [120, 100, 150, 180];
+				ap = [4, 6, 8, 10];
+				z = "0";
+				w = "0";
+				a = 0;
+				apb = 0;
+				win = 0;
+				
+				document.getElementById("1").style.opacity = "1";
+				document.getElementById("2").style.opacity = "1";
+				document.getElementById("3").style.opacity = "1";
+				document.getElementById("4").style.opacity = "1";
+				document.getElementById("5").style.opacity = "0";
+				document.getElementById("6").style.opacity = "0";
+				document.getElementById("7").style.opacity = "0";
+				document.getElementById("8").style.opacity = "0";
+
+					for (var i = 0; i < hp.length; i++) {
+						var html = "<p>" + hp[i] + "</p>";
+						document.querySelector(".h" + (i + 1)).innerHTML = html;
+					}
+
+					for (i = 0; i < hp.length; i++) {
+						var htmll = "<p>" + hp[i] + "</p>";
+						document.querySelector(".h" + (i + 5)).innerHTML = htmll;
+					}
+
+					htmlll = "<p></p>";
+					document.querySelector("#gameover").innerHTML = htmlll;
+
+					var htmlllll = "<p></p>";
+					document.querySelector("#pointText").innerHTML = htmlllll;
+			});
+	}
+
+	if (hp[x] >= 0) {
 		hp[y] = hp[y] - cap[x];
 	}
 			
-	if (hp[y] < 0) {
+	if (hp[y] <= 0) {
 		hp[y] = 0;
 			var htmlll = "<p>Game Over. You lose.</p>";
 			document.querySelector("#gameover").innerHTML = htmlll;
 
 			var htmllll = "<button class='btn btn-primary'>Restart</button>"
 			document.querySelector("#restartButton").innerHTML = htmllll;
+
+			$("#restartButton").on("click",function() {
+				hp = [120, 100, 150, 180];
+				ap = [4, 6, 8, 10];
+				z = "0";
+				w = "0";
+				a = 0;
+				apb = 0;
+				win = 0;
+				
+				document.getElementById("1").style.opacity = "1";
+				document.getElementById("2").style.opacity = "1";
+				document.getElementById("3").style.opacity = "1";
+				document.getElementById("4").style.opacity = "1";
+				document.getElementById("5").style.opacity = "0";
+				document.getElementById("6").style.opacity = "0";
+				document.getElementById("7").style.opacity = "0";
+				document.getElementById("8").style.opacity = "0";
+
+					for (var i = 0; i < hp.length; i++) {
+						var html = "<p>" + hp[i] + "</p>";
+						document.querySelector(".h" + (i + 1)).innerHTML = html;
+					}
+
+					for (i = 0; i < hp.length; i++) {
+						var htmll = "<p>" + hp[i] + "</p>";
+						document.querySelector(".h" + (i + 5)).innerHTML = htmll;
+					}
+
+					htmlll = "<p></p>";
+					document.querySelector("#gameover").innerHTML = htmlll;
+
+					var htmlllll = "<p></p>";
+					document.querySelector("#pointText").innerHTML = htmlllll;
+			});
 	}
 
 	for (var i = 0; i < hp.length; i++) {
@@ -96,7 +182,7 @@ function Attack() {
 		document.querySelector(".h" + (i + 5)).innerHTML = htmll;
 	}
 
-	var htmlllll = "<p>" + hp[y] + "</p><p>" + hp[x] + "</p>";
+	var htmlllll = "<p>You attacked " + characters[x] + " for <b>" + (ap[y] * apb) + "</b> damage.</p><p>" + characters[x] + " attacked you back for <b>" + cap[x] + "</b> damage.</p>";
 	document.querySelector("#pointText").innerHTML = htmlllll;
 
 }
